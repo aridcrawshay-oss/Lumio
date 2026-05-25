7b30031ee4ead21c37674d970cc91218ee4839b2
Ari Crawshay <aricrawshay@Aris-MacBook-Air-2.local>
2026-05-26 00:09:49 +1000

    Restore server client

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,15 +1,28 @@
 import { createClient } from '@supabase/supabase-js'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import {
+  createClientComponentClient,
+  createRouteHandlerClient,
+} from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 import type { Database } from '@/types/database'
 
 // Browser client
 export const createBrowserClient = () =>
   createClientComponentClient<Database>()
 
-// Admin client - only use in API routes
+// Server client
+export const createServerClient = () =>
+  createRouteHandlerClient<Database>({ cookies })
+
+// Admin client
 export const createAdminClient = () =>
   createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    { auth: { autoRefreshToken: false, persistSession: false } }
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    }
   )
